8909396f7d167a7b4fa7acb7c52717fd5ba92907
unknown <arkadiuszhorwat@gmail.com>
2021-03-29 19:41:43 +0200

    create infinite scroll and fetch new data

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import CharactersList from './components/CharactersList';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const peopleAPI = "https://swapi.dev/api/people/?page=1";//count:1-83, pages:9
-//pages: 1-8: results[0-9], page: 9: results[0-1]
+const apiURL = 'https://swapi.dev/api/people/?page=1';
 
 function App() {
   const [ characters, setCharacters ] = useState([]);
+  const [ nextPage, setNextPage ] = useState('');
 
   useEffect(() => {
-    getCharacters(peopleAPI);
+    getCharacters(apiURL);
   }, [])
 
   const getCharacters = apiURL => {
-    fetch(apiURL)
+      fetch(apiURL)
       .then(response => response.json())
       .then( data => {
-        setCharacters(data.results);
+        setCharacters(data.results);  
+        setNextPage(data.next);
       });
   }
 
-  console.log(characters)
+  const loadeCharacters = () => {
+    fetch(nextPage)
+    .then(response => response.json())
+    .then( data => {
+      data.results.map( result => {
+        setCharacters( prev => [...prev, result]);  
+      })
+      setNextPage(data.next);
+    });
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Star Wars Catalogue</h1>
       </header>
+      {
+        nextPage != null &&  <InfiniteScroll
+          dataLength={characters.length}
+          hasMore={true}
+          next={loadeCharacters}
+        >  
+        </InfiniteScroll>
+      }
       <ul>
-        {characters.length > 0 && characters.map( character => (
-          <CharactersList 
-            key={character.name} 
-            {...character}
-          />
-        ))}
-      </ul>
+          {characters.length > 0 && characters.map( character => (
+            <CharactersList 
+              key={character.name} 
+              {...character}
+            />
+          ))}
+        </ul>
     </div>
   );
 }
--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -3,30 +3,29 @@ import React, {useState, useEffect} from 'react';
 const CharactersList = ( {name,gender,birth_year,mass,height, films} ) => {
     const [movie, setMovies] = useState([]);
 
-
     useEffect(() => {
         setMovies([]);
 
         films.map(film => {
-            fetch(film)
-                .then( response => response.json())
-                .then( data => {
-                    setMovies(prev => [...prev, data.title]);
-                })
-        })
-    },[])
+        fetch(film)
+            .then( response => response.json())
+            .then( data => {
+                setMovies(prev => [...prev, data.title]);
+            });
+        });
+    },[]);
 
     return (
         <>
         <li>
-            <p>{name}</p>
-            <p>{gender}</p>
-            <p>{birth_year}</p>
+            <p>Name: {name}</p>
+            <p>Gender: {gender}</p>
+            <p>Birth Year: {birth_year}</p>
         </li>
         <div>
-            <p>{mass}</p>
-            <p>{height}</p>
-            <p>{movie}</p>
+            <p>Mass: {mass}</p>
+            <p>Height: {height}</p>
+            <p>Movies Titles: {`${movie}`}</p>
         </div>
         </>
     )
